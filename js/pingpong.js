6674bc86157a52e5f7cec0e074d4a01970dee920
boon26 <41929@qq.com>
2018-07-31 15:30:47 +0800

    version1.0

--- js/pingpong.js
+++ js/pingpong.js
@@ -17,7 +17,11 @@ pingpong.ball={
 	directionX:1,
 	directionY:1
 };
-
+//
+var sc={
+	scoreA:0,
+	scoreB:0
+};
 //
 $(function(){
 	$("#paddleB").css("top",20);
@@ -65,8 +69,8 @@ function moveBall(){
 	var playgroundHeight=parseInt($("#playground").height());
 	var playgroundWidth=parseInt($("#playground").width());
 	var ball=pingpong.ball;
-	//
-	//
+	/*
+	//小球到达边界反弹
 	if(ball.y+ball.speed*ball.directionY>playgroundHeight){
 		ball.directionY=-1;
 	}
@@ -79,8 +83,49 @@ function moveBall(){
 	if(ball.x+ball.speed*ball.directionX<0){
 		ball.directionX=1;
 	}
+	*/
+	//检测小球是否出界
+	//检测右边界
+	if(ball.x+ball.speed*ball.directionX>playgroundWidth){
+		//玩家B丢分
+		sc.scoreA++;
+		$("#scoreA").html(sc.scoreA);
+		//重置小球
+		ball.x=250;
+		ball.y=100;
+		$("#ball").css({"left":ball.x,"top":ball.y});
+		ball.directionX=-1;
+	}
+	//检测左边界
+	if(ball.x+ball.speed*ball.directionX<0){
+		//玩家A丢分
+		sc.scoreB++;
+		$("#scoreB").html(sc.scoreB);
+		//重置小球
+		ball.x=150;
+		ball.y=100;
+		$("#ball").css({"left":ball.x,"top":ball.y});
+		ball.directionX=1;
+	}
 	//检测球拍
-	
+	//检测左边球拍
+	var paddleAX=parseInt($("#paddleA").css("left"))+parseInt($("#paddleA").css("width"));
+	var paddleAYBottom=parseInt($("#paddleA").css("top"))+parseInt($("#paddleA").css("height"));
+	var paddleAYTop=parseInt($("#paddleA").css("top"));
+	if(ball.x+ball.speed*ball.directionX<paddleAX){
+		if(ball.y+ball.speed*ball.directionY<=paddleAYBottom && ball.y+ball.speed*ball.directionY>=paddleAYTop){
+			ball.directionX=1;
+		}
+	}
+	//检测右边球拍
+	var paddleBX=parseInt($("#paddleB").css("left"))-parseInt($("#ball").css("width"));
+	var paddleBYBottom=parseInt($("#paddleB").css("top"))+parseInt($("#paddleB").css("height"));
+	var paddleBYTop=parseInt($("#paddleB").css("top"));
+	if(ball.x+ball.speed*ball.directionX>paddleBX){
+		if(ball.y+ball.speed*ball.directionY<=paddleBYBottom && ball.y+ball.speed*ball.directionY>=paddleBYTop){
+			ball.directionX=-1;
+		}
+	}
 	//移动小球
 	ball.x+=ball.speed*ball.directionX;
 	ball.y+=ball.speed*ball.directionY;
